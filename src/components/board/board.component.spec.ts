cd6ac04eb9beace7215cbb380ffa36a42ccc15b3
Naman Vinod <namanvinod1@gmail.com>
2021-10-02 21:46:46 +0530

    Added Squares on Board

--- src/components/board/board.component.spec.ts
+++ src/components/board/board.component.spec.ts
@@ -22,4 +22,32 @@ describe('BoardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render 3 board rows', () => {
+    fixture.detectChanges();
+    const htmlElement = fixture.nativeElement as HTMLElement;
+    expect(htmlElement.querySelector('.board-row')).not.toBe(null);
+    expect(htmlElement.querySelectorAll('.board-row').length).toBe(3);
+  });
+
+  it('should render 3 squares in first row', () => {
+    fixture.detectChanges();
+    const htmlElement = fixture.nativeElement as HTMLElement;
+    expect(htmlElement.querySelector('.board-row')?.querySelector('square')).not.toBe(null);
+    expect(htmlElement.querySelector('.board-row')?.querySelectorAll('square').length).toBe(3);
+  });
+
+  it('should render 3 squares in second row', () => {
+    fixture.detectChanges();
+    const htmlElement = fixture.nativeElement as HTMLElement;
+    expect(htmlElement.querySelectorAll('.board-row')[1]?.querySelector('square')).not.toBe(null);
+    expect(htmlElement.querySelectorAll('.board-row')[1]?.querySelectorAll('square').length).toBe(3);
+  });
+
+  it('should render 3 squares in third row', () => {
+    fixture.detectChanges();
+    const htmlElement = fixture.nativeElement as HTMLElement;
+    expect(htmlElement.querySelectorAll('.board-row')[2]?.querySelector('square')).not.toBe(null);
+    expect(htmlElement.querySelectorAll('.board-row')[2]?.querySelectorAll('square').length).toBe(3);
+  });
 });
