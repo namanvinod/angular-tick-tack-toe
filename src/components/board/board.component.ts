ab7aca7cb19a8c5da24ca184343c57d5bc7b9595
Naman Vinod <namanvinod1@gmail.com>
2021-10-02 21:34:23 +0530

    Added Board Component

---
+++ src/components/board/board.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BoardComponent { }
